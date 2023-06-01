e99a821eaa18fcb1cb018fcf84e597c9b8f043e3
Daniel Mack <>
2023-06-01 16:03:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# `poker-lookup-table`
+
+This crate is meant to regenerate the static lookup table used by the [`poker`](https://github.com/deus-x-mackina/poker)
+crate. This binary is not meant to be published on crates.io; the repo is mainly meant to keep the large static lookup table
+out of the main repository.
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+// Note: make lookup table public in poker src in order to run
+
+use std::{
+    fs::File,
+    io::{prelude::*, BufWriter, Result},
+};
+
+use poker::Evaluator;
+
+fn main() -> Result<()> {
+    let mut file = File::create("codegen.txt").map(BufWriter::new)?;
+
+    let table = Evaluator::new().0;
+
+    // Flush lookup
+    write!(
+        &mut file,
+        "use crate::{{evaluate::{{hand_rank::PokerHandRank, meta::Meta::{{self, *}}}}, \
+         Rank::*}};\n\nstatic FLUSH_LOOKUP: ::phf::Map<i32, Meta> = "
+    )?;
+    let mut builder = &mut phf_codegen::Map::new();
+    for (&key, &value) in table.flush_lookup.iter() {
+        builder = builder.entry(key, &format!("{:?}", value));
+    }
+    write!(&mut file, "{};\n\n", builder.build())?;
+
+    // Unsuited lookup
+    write!(
+        &mut file,
+        "static UNSUITED_LOOKUP: ::phf::Map<i32, Meta> = "
+    )?;
+    let mut builder = &mut phf_codegen::Map::new();
+    for (&key, &value) in table.unsuited_lookup.iter() {
+        builder = builder.entry(key, &format!("{:?}", value));
+    }
+    write!(&mut file, "{};", builder.build())?;
+
+    Ok(())
+}
