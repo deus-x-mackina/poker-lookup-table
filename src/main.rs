2a56cec6dab0822156e7cb9c857313cbe97c74e3
Daniel Mack <>
2025-01-11 15:48:38 -0600

    Add 3-card lookup tables

--- src/main.rs
+++ src/main.rs
@@ -10,30 +10,56 @@ use poker::Evaluator;
 fn main() -> Result<()> {
     let mut file = File::create("codegen.txt").map(BufWriter::new)?;
 
-    let table = Evaluator::new().0;
+    let e = Evaluator::new();
+    let five = e.five_card_table;
+    let three = e.three_card_table;
 
-    // Flush lookup
+    // Imports
+    write!(&mut file, "use crate::evaluate::eval::Eval;\n\n")?;
+
+    // Flush lookup (5 card)
     write!(
         &mut file,
-        "use crate::{{evaluate::{{hand_rank::PokerHandRank, meta::Meta::{{self, *}}}}, \
-         Rank::*}};\n\npub static FLUSH_LOOKUP: ::phf::Map<i32, Meta> = "
+        "pub static FLUSH_LOOKUP_FIVE: ::phf::Map<i32, Eval<crate::evaluate::poker_type::FiveCard>> = "
     )?;
     let mut builder = &mut phf_codegen::Map::new();
-    for (&key, &value) in table.flush_lookup.iter() {
-        builder = builder.entry(key, &format!("{:?}", value));
+    for (&key, &value) in five.flush_lookup.iter() {
+        builder = builder.entry(key, &format!("Eval::new({}, {})", value.hand_rank(), value.rank_flags()));
     }
     write!(&mut file, "{};\n\n", builder.build())?;
 
-    // Unsuited lookup
+    // Unsuited lookup (5 card)
     write!(
         &mut file,
-        "pub static UNSUITED_LOOKUP: ::phf::Map<i32, Meta> = "
+        "pub static UNSUITED_LOOKUP_FIVE: ::phf::Map<i32, Eval<crate::evaluate::poker_type::FiveCard>> = "
     )?;
     let mut builder = &mut phf_codegen::Map::new();
-    for (&key, &value) in table.unsuited_lookup.iter() {
-        builder = builder.entry(key, &format!("{:?}", value));
+    for (&key, &value) in five.unsuited_lookup.iter() {
+        builder = builder.entry(key, &format!("Eval::new({}, {})", value.hand_rank(), value.rank_flags()));
     }
-    write!(&mut file, "{};", builder.build())?;
+    write!(&mut file, "{};\n\n", builder.build())?;
+
+    // Flush lookup (3 card)
+    write!(
+        &mut file,
+        "pub static FLUSH_LOOKUP_THREE: ::phf::Map<i32, Eval<crate::evaluate::poker_type::FiveCard>> = "
+    )?;
+    let mut builder = &mut phf_codegen::Map::new();
+    for (&key, &value) in three.flush_lookup.iter() {
+        builder = builder.entry(key, &format!("Eval::new({}, {})", value.hand_rank(), value.rank_flags()));
+    }
+    write!(&mut file, "{};\n\n", builder.build())?;
+
+    // Unsuited lookup (5 card)
+    write!(
+        &mut file,
+        "pub static UNSUITED_LOOKUP_THREE: ::phf::Map<i32, Eval<crate::evaluate::poker_type::FiveCard>> = "
+    )?;
+    let mut builder = &mut phf_codegen::Map::new();
+    for (&key, &value) in three.unsuited_lookup.iter() {
+        builder = builder.entry(key, &format!("Eval::new({}, {})", value.hand_rank(), value.rank_flags()));
+    }
+    write!(&mut file, "{};\n\n", builder.build())?;
 
     Ok(())
 }
